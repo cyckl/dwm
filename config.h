cfa7e9acc75d8642e6579a82cb3c0397986780ac
cyckl <git@cyckl.net>
2022-01-05 21:23:22 -0800

    add custom bar height support

--- config.h
+++ config.h
@@ -4,6 +4,7 @@
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int gap       = 16;       /* gap size in px */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const int barheight          = 24;        /* bar height in px, 0 = auto */ 
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Terminus:size=12" };
