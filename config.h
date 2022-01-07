fcb9cf215c811c4d9c96d374d5ee6a4802880ae9
cyckl <git@cyckl.net>
2022-01-06 17:53:08 -0800

    fix dmenu color error

--- config.h
+++ config.h
@@ -57,7 +57,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_bar, "-nf", col_fg, "-sb", col_accent, "-sf", col_fg, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_bar, "-nf", col_fg, "-sb", col_fg, "-sf", col_bar, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
